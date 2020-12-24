aab702ff90a0861b5a21fd9fba30bf0160019219
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-24 22:28:28 +0530

    Add : stake and bet value per day for gambler

--- GamblingSimulation.js
+++ GamblingSimulation.js
@@ -1,1 +1,10 @@
-console.log(`\n****** Welcome to Gambling Simulation Problem *******\n`);
+const gamblersimulator = require('./GamblingSimulator');
+
+class GamblingSimulationMain {
+    gambler = () => {
+        console.log(`\n****** Welcome to Gambling Simulation Problem *******\n`);
+        gamblersimulator.simulation();
+    }
+}
+const object = new GamblingSimulationMain();
+object.gambler();
---
+++ GamblingSimulator.js
@@ -0,0 +1,12 @@
+
+class GamblingSimulator {
+simulation = () => {
+    var stake = 100;
+    var day = 1;
+    var winStake = 100;
+    var lostStake = 100;
+    console.log(`Gambler starts with stake of $100 and bet $1 every day`);
+
+}
+}
+module.exports = new GamblingSimulator();
