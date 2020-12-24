de901977cf43a0ff18682de160876b07c7f0f518
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-25 00:24:49 +0530

    Add : days win or loss in month and by how much

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -8,18 +8,25 @@ class GamblingSimulator {
         console.log(`------ Gambler starts with stake of $100 and bet $1 every day ------`);
 
         var getRandomValue = () => Math.floor(Math.random() * 2);
-        while (day <= 20) {
+        while (day <= 30) {
             if (getRandomValue() == 0) {
                 lostStake = lostStake - 1;
-                console.log(`amount after loose for a day is: ${lostStake}`);
+                console.log(`amount after loose for a day ${day} is: ${lostStake}`);
             }
             else {
                 winStake = winStake + 1;
-                console.log(`amount after win for a day is: ${winStake}`);
+                console.log(`amount after win for a day ${day} is: ${winStake}`);
             }
             var winAmount = winStake - stake;
             var lostAmount = stake - lostStake;
             console.log(`Win amount of day ${day} is: ${winAmount} and Lost amount of day ${day}  is: ${lostAmount}`);
+
+            if (winAmount > lostAmount) {
+                console.log("Day" + day + " win by " + (winAmount - lostAmount));
+            }
+            else {
+                console.log("Day" + day + " lost by " + (lostAmount - winAmount));
+            }
             day++;
         }
     }
