1fa332e2033600dfbc432554dc83108194147d00
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-24 23:37:35 +0530

    feat : 50 % loose or win and resign of the day

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -5,20 +5,26 @@ class GamblingSimulator {
         var day = 1;
         var winStake = 100;
         var lostStake = 100;
-        console.log(`Gambler starts with stake of $100 and bet $1 every day`);
+        console.log(`------ Gambler starts with stake of $100 and bet $1 every day ------`);
 
         var getRandomValue = () => Math.floor(Math.random() * 2);
-        console.log(getRandomValue());
-        if (getRandomValue() == 0) {
-            lostStake = lostStake - 1;
-            console.log(`********Gambler Loose********** `);
-            console.log(`amount is: ${lostStake}`);
+
+        while (winStake < 150 || lostStake > 50) {
+
+            if (getRandomValue() == 0) {
+                lostStake = lostStake - 1;
+                console.log(`amount after loose for a day is: ${lostStake}`);
+            }
+            else {
+                winStake = winStake + 1;
+                console.log(`amount after win for a day is: ${winStake}`);
+            }
         }
-        else {
-            winStake = winStake + 1;
-            console.log(`********Gambler Win**********`);
-            console.log(`amount is: ${winStake}`);
-        }
+        if (winStake == 150)
+            console.log(`Gambler won 50 % of stake`);
+        else
+            console.log(`Gambler loose 50 % of stake`);
     }
 }
+
 module.exports = new GamblingSimulator();
